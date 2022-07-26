be7c72a8779319f049316dee281158a2965900d1
pankaj.kumar16 <panakj.kumar@mowito.in>
2022-07-26 22:50:03 +0530

    adding py file

---
+++ scripts/serial.py
@@ -0,0 +1,15 @@
+i=0
+while i<10:
+    i=i+1
+    if i==1:
+        print("hey pankaj")
+    elif i==2:
+        print("i am happy to see you back in your action")
+    elif i==3:
+        print("i will inform to the system to have their system ready to heatup :)")
+    elif i==4 or i==5 or i==6:
+        print("let`s goo.....")
+    elif i==7 or i==8:
+        print("goo.....")
+    else:
+        print("getting it done.. ")
