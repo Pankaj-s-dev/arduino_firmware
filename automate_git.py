45aee254f3a635ad8cc1bfcf2d78df23f8afddde
pankaj <pankaj.kumar@mowito.in>
2023-02-21 01:08:39 +0530

    commit message from python script

--- automate_git.py
+++ automate_git.py
@@ -33,8 +33,8 @@ def repo_creater(repo):
             line = file1.readline()
     
         # if line is empty
+        print(line[0])
         if not line:
-            print("gdggggggggggggg")
             repo.git.checkout('-b', getpass.getuser())
             with open(f'{git_response_file}', 'a', encoding='utf-8') as file1:
                 file1.writelines("True")
