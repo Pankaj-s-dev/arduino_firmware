8f88c4564680206f9cdd8204cf3dc38bf65663d0
pankaj <pankaj.kumar@mowito.in>
2023-02-20 23:15:50 +0530

    commit message from python script

--- automate_git.py
+++ automate_git.py
@@ -1,7 +1,9 @@
 import  git 
+import os
+
 
 from git import Repo
-existing_repo = Repo('/home/logi/dev_ws/arduino_firmware')
+existing_repo = Repo(os.getcwd())
 
 existing_repo.git.add('--all')
 existing_repo.git.commit('-m', 'commit message from python script')
