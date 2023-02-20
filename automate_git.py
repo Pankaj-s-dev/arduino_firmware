aa92bd1ddcf2d9a6670cd674dd12b385bda17005
pankaj <pankaj.kumar@mowito.in>
2023-02-20 23:23:38 +0530

    commit message from python script

--- automate_git.py
+++ automate_git.py
@@ -6,9 +6,10 @@ existing_repo = Repo(os.getcwd())
 
 if existing_repo.git.diff():
     print("Yes")
+    print(existing_repo.git.add('--all'))
+    print(existing_repo.git.commit('-m', 'commit message from python script'))
+    origin = existing_repo.remote(name='origin')
+    print(origin.push())
 else:
-    print("No Diff")
-print(existing_repo.git.add('--all'))
-print(existing_repo.git.commit('-m', 'commit message from python script'))
-origin = existing_repo.remote(name='origin')
-print(origin.push())
+    print("Everyting is up to dated")
+
