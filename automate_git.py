d447a8a675b6bf9df6cc9f8a706767cee15b02c7
pankaj <pankaj.kumar@mowito.in>
2023-02-20 23:17:46 +0530

    commit message from python script

--- automate_git.py
+++ automate_git.py
@@ -1,11 +1,10 @@
 import  git 
 import os
 
-
 from git import Repo
 existing_repo = Repo(os.getcwd())
 
-existing_repo.git.add('--all')
-existing_repo.git.commit('-m', 'commit message from python script')
+print(existing_repo.git.add('--all'))
+print(existing_repo.git.commit('-m', 'commit message from python script'))
 origin = existing_repo.remote(name='origin')
-origin.push()
+print(origin.push())
